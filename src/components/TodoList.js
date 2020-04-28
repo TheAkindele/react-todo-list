7b8d55e1a437d41f7e406a7b9fd859bfd698f954
Akindele Muhyideen <disgcfrguy@gmail.com>
2020-04-28 08:51:15 +0100

    the components folder

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default TodoList
